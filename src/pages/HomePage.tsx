b4a2a3f5a9421a980cfece747260a57f05d66089
AliJafaripour <alijafaripour82@gmail.com>
2025-03-20 04:38:34 +0330

    Feat: Motion For Timeline Img

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Footer, Header,Test } from '../components';
 import { Timeline } from '../components/ui/timeline';
+import { motion } from 'framer-motion';
 
 
 
@@ -24,48 +25,94 @@ export default HomePage;
 
 
 
+const imageAnimations = {
+  container: {
+    hidden: { opacity: 0 },
+    visible: { 
+      opacity: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  },
+  item: {
+    hidden: { opacity: 0, y: 90, scale: 0.92 },
+    visible: { 
+      opacity: 1, 
+      y: 10, 
+      scale: 1,
+      transition: {
+        duration: 2.5,
+        ease: [0.25, 0.1, 0.25, 1],
+      }
+    }
+  }
+};
+
+
+
+
+
 export function TimelineDemo() {
   const data = [
     {
       title: "افتتاحیه",
       content: (
         <div>
-          <p className=" text-primary-1 text-xs md:text-sm font-normal mb-8 text-right">
-           یه جمله در باره ی افتتحایه مراسم
+          <p className="text-primary-1 text-xs md:text-sm font-normal mb-8 text-right">
+            یه جمله در باره ی افتتحایه مراسم
           </p>
-          <p className=" text-primary-1 text-xs md:text-sm font-normal mb-8 text-right">
-          یه سری چرت و پرت دیگ برای افتتاحیه این رویداد 
+          <p className="text-primary-1 text-xs md:text-sm font-normal mb-8 text-right">
+            یه سری چرت و پرت دیگ برای افتتاحیه این رویداد 
           </p>
-          <div className="grid grid-cols-2 gap-4">
-            <img
+          <motion.div 
+            variants={imageAnimations.container}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, margin: "-100px", amount:0.1 }}
+            className="grid grid-cols-2 gap-4"
+          >
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ef5925.JPG"
               alt="hero template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-            <img
+
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ef2239 (1).JPG"
-              alt="feature template"
+              alt="hero template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-            <img
+            
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ef2239 (2).JPG"
               alt="bento template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ef2239 (3).JPG"
               alt="cards template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-          </div>
+
+
+            
+          </motion.div>
+
+
         </div>
       ),
     },
@@ -93,17 +140,28 @@ export function TimelineDemo() {
              مسابقه برنامه نویسی✅
             </div>
           </div>
-          <div className="h-72 lg:h-auto grid grid-cols-10  grid-rows-7  gap-3 lg:gap-6">
+
+
+
+
+          <motion.div 
+            variants={imageAnimations.container}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, margin: "-100px", amount:0.1}}
+            className="h-72 lg:h-auto grid grid-cols-10  grid-rows-7  gap-3 lg:gap-6">
 
            
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_G2753.JPG"
               alt="startup template"
               width={500}
               height={500}
               className="col-span-5 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_G3336.JPG"
               alt="startup template"
               width={500}
@@ -113,49 +171,54 @@ export function TimelineDemo() {
 
 
 
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_6732.JPG"
               alt="startup template"
               width={500}
               height={500}
-              className="col-span-3 row-span-3 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full  "
+              className="col-span-3 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full  "
             />
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_2863.JPG"
               alt="startup template"
               width={500}
               height={500}
-              className="col-span-4 row-span-3 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
+              className="col-span-4 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
-            <img
+           <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_3463.JPG"
               alt="startup template"
               width={500}
               height={500}
-              className="col-span-3 row-span-3 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
+              className="col-span-3 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
 
 
 
 
 
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_G6244.jpg"
               alt="startup template"
               width={500}
               height={500}
-              className="col-span-5 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
+              className="col-span-5 row-span-3 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
 
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_G6435.JPG"
               alt="startup template"
               width={500}
               height={500}
-              className="col-span-5 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
+              className="col-span-5 row-span-3 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
             
-          </div>
+            </motion.div>
         </div>
       ),
     },
@@ -170,17 +233,24 @@ export function TimelineDemo() {
 
 
          
-          <div className="h-72 lg:h-auto grid grid-cols-10  grid-rows-7  gap-3 lg:gap-6">
+          <motion.div 
+            variants={imageAnimations.container}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, margin: "-100px", amount:0.1}}
+            className="h-72 lg:h-auto grid grid-cols-10  grid-rows-7  gap-3 lg:gap-6">
 
            
-            <img
+          <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ek3578.JPG"
               alt="startup template"
               width={500}
               height={500}
               className="col-span-5 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ek3578.JPG"
               alt="startup template"
               width={500}
@@ -190,14 +260,16 @@ export function TimelineDemo() {
 
 
 
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ek6946.JPG"
               alt="startup template"
               width={500}
               height={500}
               className="col-span-7 row-span-3 rounded-lg object-cover h-full md:h-44 lg:h-auto w-full  "
             />
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ek2485.JPG"
               alt="startup template"
               width={500}
@@ -206,7 +278,8 @@ export function TimelineDemo() {
             />
 
 
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ek3578.JPG"
               alt="startup template"
               width={500}
@@ -214,7 +287,8 @@ export function TimelineDemo() {
               className="col-span-5 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
 
-            <img
+            <motion.img
+              variants={imageAnimations.item}
               src="../../public/images/IMG_ek3578.JPG"
               alt="startup template"
               width={500}
@@ -222,7 +296,7 @@ export function TimelineDemo() {
               className="col-span-5 row-span-2 rounded-lg object-cover h-full md:h-44 lg:h-60 w-full "
             />
             
-          </div>
+            </motion.div>
 
         </div>
       ),
@@ -231,7 +305,6 @@ export function TimelineDemo() {
 
   return (
     <div className="w-full relative">
-      {/* تایم‌لاین */}
       <Timeline data={data} />
     </div>
   );
